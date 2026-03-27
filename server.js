df720813befea56be5a05b880e34ad74e9b890db
SreeKeerthana12 <sreekeerthana210@gmail.com>
2026-03-27 15:14:21 +0530

    force update server

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const app = express();
 app.use(express.raw({ type: 'image/jpeg', limit: '10mb' }));
 
 app.get('/', (req, res) => {
-    res.send("Server running");
+    res.send("Server running - UPDATED");
 });
 
 app.post('/upload', (req, res) => {
     console.log("Image received, size:", req.body.length);
-    res.send("OK");
+    res.send("UPLOAD WORKING");
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log("Server running on port", PORT);
 });
